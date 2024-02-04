4ed361d71b569e78c94631488685037524ec2c6a
kristian roopnarine <kristian.roopnarine@gmail.com>
2024-02-04 00:10:29 -0500

    read characters from input file and save into buffer

--- main.c
+++ main.c
@@ -9,9 +9,40 @@ char *read_argv_params(int, char *[]);
 int main(int argc, char *argv[])
 {
     FILE *fd;
+    size_t index = 0;
+    size_t size = 0;
+    char *buffer = (char *)malloc(sizeof(char) * CHUNK_SIZE), *tmp = (char *)malloc(sizeof(char) * CHUNK_SIZE);
     char *file_name = (char *)malloc(sizeof(char) * CHUNK_SIZE);
     file_name = read_argv_params(argc, argv);
-    printf(" %s", file_name);
+    fd = fopen(file_name, "r");
+    if (fd == NULL)
+    {
+        printf("Error opening file");
+        return 0;
+    }
+    int ch = EOF;
+    while (ch)
+    {
+        ch = getc(fd);
+        if (ch == EOF)
+        {
+            ch = 0;
+        }
+        if (size <= index)
+        {
+            size += CHUNK_SIZE;
+            tmp = realloc(buffer, size);
+            if (!tmp)
+            {
+                free(buffer);
+                buffer = NULL;
+                break;
+            }
+            buffer = tmp;
+        }
+        buffer[index++] = ch;
+    }
+    free(tmp);
 }
 
 char *read_argv_params(int argc, char *argv[])
