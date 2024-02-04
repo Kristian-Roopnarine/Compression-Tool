f844edd43e033f72e63fe0863304fc26ef2a2b69
kristian roopnarine <kristian.roopnarine@gmail.com>
2024-02-04 00:10:29 -0500

    I am stuck, the huffman tree does not seem to be correct

--- main.c
+++ main.c
@@ -10,8 +10,8 @@ typedef struct HuffmanLeaf
     char letter;
     int frequency;
     bool is_internal_node;
-    struct huffman_leaf *left;
-    struct huffman_leaf *right;
+    struct HuffmanLeaf *left;
+    struct HuffmanLeaf *right;
 } huffman_leaf;
 
 int CHUNK_SIZE = 16 * 1024;
@@ -22,6 +22,10 @@ int right_child(int i);
 void min_heapify(huffman_leaf a[], int i, int n);
 void print_heap(huffman_leaf a[], int n);
 void check_min_heap_property(huffman_leaf a[], int n);
+void build_huffman_tree(huffman_leaf a[], int n);
+huffman_leaf dequeue(huffman_leaf a[], int *n);
+void insert(huffman_leaf a[], huffman_leaf node, int *n);
+void walk_tree(huffman_leaf tree);
 
 int main(int argc, char *argv[])
 {
@@ -73,7 +77,26 @@ int main(int argc, char *argv[])
         min_heapify(minHeap, i, items_in_heap - 1);
     }
     check_min_heap_property(minHeap, number_of_huffman_leaf);
+    build_huffman_tree(minHeap, number_of_huffman_leaf);
+    huffman_leaf HTree = minHeap[0];
+    walk_tree(HTree);
     return 0;
+}
+
+void walk_tree(huffman_leaf tree)
+{
+    if (tree.left == NULL && tree.right == NULL)
+    {
+        return;
+    }
+    if (tree.left != NULL)
+    {
+        return walk_tree(*tree.left);
+    }
+    if (tree.right != NULL)
+    {
+        return walk_tree(*tree.right);
+    }
 }
 
 char *read_argv_params(int argc, char *argv[])
@@ -161,5 +184,39 @@ void check_min_heap_property(huffman_leaf a[], int n)
             printf("Parent : %d, Right: %d", parent.frequency, a[right_idx].frequency);
             break;
         }
+    }
+}
+
+void build_huffman_tree(huffman_leaf a[], int n)
+{
+    int min_heap_boundary = n - 1;
+    while (min_heap_boundary > 0)
+    {
+        huffman_leaf left = dequeue(a, &min_heap_boundary);
+        huffman_leaf right = dequeue(a, &min_heap_boundary);
+        huffman_leaf new_node = (huffman_leaf){'\0', left.frequency + right.frequency, true, &left, &right};
+        insert(a, new_node, &min_heap_boundary);
+    }
+}
+
+huffman_leaf dequeue(huffman_leaf a[], int *n)
+{
+    huffman_leaf node = a[0];
+    a[0] = a[*n];
+    *n = *n - 1;
+    min_heapify(a, 0, *n);
+    return node;
+}
+
+void insert(huffman_leaf a[], huffman_leaf node, int *n)
+{
+    // this might not work as expected
+    *n = *n + 1;
+    a[*n] = node;
+    int i = *n - 1;
+    while (i != 0 && a[parent(i)].frequency > a[i].frequency)
+    {
+        swap(&a[parent(i)], &a[i]);
+        i = parent(i);
     }
 }
